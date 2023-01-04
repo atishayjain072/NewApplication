dea6cadafc75c6abe48661cfeec1a26174f8cdde
atishayjain072 <jain.atishay072@gmail.com>
2023-01-04 19:30:38 +0530

    Adding Spinner and Badge and MultiCategory using Route

--- src/components/News.js
+++ src/components/News.js
@@ -1,49 +1,65 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+  static defaultProps = {
+    country: 'in',
+    pageSize: 6,
+    category: 'general',
+  }
+  static propTypes = {
+     country: PropTypes.string,
+     page: PropTypes.number,
+     category: PropTypes.string,
+  }
+
   constructor() {
     super();
     this.state = {
       articles: [],
-      loading: true,
+      loading: false,
       page: 1,
     };
   }
 
   async componentDidMount() {
-    let url =
-      `https://newsapi.org/v2/top-headlines?country=in&apiKey=3a562f3198d54c619b8ee4f48ab54a6c&page=1&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3a562f3198d54c619b8ee4f48ab54a6c&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false
     });
   }
   handlePreviousChange = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3a562f3198d54c619b8ee4f48ab54a6c&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3a562f3198d54c619b8ee4f48ab54a6c&page=${
       this.state.page - 1
     }&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       page: this.state.page - 1,
       articles: parsedData.articles,
+      loading: false
     });
   };
   handleNextChange = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)) {
-    } else {
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3a562f3198d54c619b8ee4f48ab54a6c&page=${
+    if (!(this.state.page + 1 >Math.ceil(this.state.totalResults / this.props.pageSize))) {
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=3a562f3198d54c619b8ee4f48ab54a6c&page=${
         this.state.page + 1
       }&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true});
       let data = await fetch(url);
       let parsedData = await data.json();
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
+        loading: false
       });
     }
   };
@@ -51,23 +67,25 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        < h1 className="text-center">
-        NewsBear - Top Headlines
-        </h1>
-        <Spinner />    
+        <h1 className="text-center" style={{margin: '35px,0px'}} >NewsBear - Top Headlines</h1>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title}
-                  description={element.description}
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title}
+                    description={element.description}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    publishedAt={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container">
           <div className="d-flex justify-content-between">
@@ -80,7 +98,10 @@ export class News extends Component {
               &larr; Previous
             </button>
             <button
-            disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)}
+              disabled={
+                this.state.page + 1 >
+                Math.ceil(this.state.totalResults / this.props.pageSize)
+              }
               type="button"
               className="btn btn-dark"
               onClick={this.handleNextChange}
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, publishedAt, source } =
+      this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>
+                {source}
+              </span>
           <img
             src={
               !imageUrl
@@ -16,13 +20,17 @@ export class NewsItem extends Component {
             alt="..."
           />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">
+              {title}
+            </h5>
             <p className="card-text">{description}</p>
-            <a
-              href={newsUrl}
-              rel="nonreferrer"
-              className="btn btn-sm btn-dark"
-            >
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(publishedAt).toGMTString()}{" "}
+              </small>
+            </p>
+            <a href={newsUrl} rel="nonreferrer" className="btn btn-sm btn-dark">
               Read More
             </a>
           </div>
